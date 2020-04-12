9e7f9327e7850ec8f5aa08154d7bc16485cf5ceb
adimaryadi <adi.maryadi@dwidasa.com>
2020-04-12 11:16:17 +0700

    Create an infinite scrolling ListView

--- lib/main.dart
+++ lib/main.dart
@@ -16,24 +16,49 @@ class MyApp extends StatelessWidget {
           title: Text('Selamat Datang Belajar Flutter'),
         ),
         body: Center(
-          child: RandomWords(),
+          child: _buildSuggestions(),
         ),
       )
     );
   }
 }
 
-class RandomWords extends StatefulWidget {
-  @override
-  RandomWordsState createState() => RandomWordsState();
+// class RandomWords extends StatefulWidget {
+//   @override
+//   RandomWordsState createState() => RandomWordsState();
+// }
+
+// class RandomWordsState extends State<RandomWords> {
+//   @override
+//   Widget build(BuildContext context) {
+//     final wordPair   =   WordPair.random();
+//     return Text(wordPair.asPascalCase);
+//   }
+// }
+
+Widget _buildSuggestions() {
+  final _suggestions    =    <WordPair>[];
+  return ListView.builder(
+    padding: const EdgeInsets.all(16),
+    itemBuilder: (context, i) {
+      if (i.isOdd) return Divider();
+      final index = i ~/ 2;
+      if (index >= _suggestions.length) {
+        _suggestions.addAll(generateWordPairs().take(4000));
+      }
+      return _buildRow(_suggestions[index]);
+    },
+  );
 }
 
-class RandomWordsState extends State<RandomWords> {
-  @override
-  Widget build(BuildContext context) {
-    final wordPair   =   WordPair.random();
-    return Text(wordPair.asPascalCase);
-  }
+Widget _buildRow(WordPair pair) {
+  final _biggerFont   =   const TextStyle(fontSize: 24);
+  return ListTile(
+    title: Text(
+      pair.asPascalCase,
+      style: _biggerFont
+    ),
+  );
 }
 
 class MyHomePage extends StatefulWidget {
