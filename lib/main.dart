6079b5bb04c3508a1bccd987d853bd279479ef06
adimaryadi <adi.maryadi@dwidasa.com>
2020-04-28 14:53:31 +0700

    Animate a widget across screens

--- lib/main.dart
+++ lib/main.dart
@@ -9,53 +9,47 @@ void main() {
 class MyApp extends StatelessWidget {
     Widget build(BuildContext context) {
       return Scaffold(
-        appBar: AppBar(),
-        body: Center(
-          child: RaisedButton(
-            child: Text('Go!'),
-            onPressed: () {
-               Navigator.of(context).push(_createRoute());
-            },
+        appBar: AppBar(
+          title: Text("Main Screen"),
+        ),
+        body: GestureDetector(
+          child: Hero(
+            tag: 'Image Laptop',
+            child: Image.network(
+              'https://picsum.photos/250?image=9'
+            ),
           ),
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (_) { 
+                return DetailScreen();
+              })
+            );
+          },
         ),
       );
     }
 }
 
-Route _createRoute() {
-  return PageRouteBuilder(
-    pageBuilder: (context, animation, secondaryAnimation) => PageTwo(),
-    transitionsBuilder: (context, animation, secondaryAnimation, child) {
-      var begin     =   Offset(0.0, 1.0);
-      var end       =   Offset.zero;
-      var curve     =   Curves.ease;
-
-      var tween              =   Tween(begin: begin,end: end);
-      var curvedAnimation    =   CurvedAnimation(
-        parent: animation,
-        curve:  curve
-      );
-
-      return SlideTransition(
-        position: tween.animate(curvedAnimation),
-        child: child,
-      );
-    },
-  );
-}
-
-
-class PageTwo extends StatelessWidget {
+class DetailScreen extends StatelessWidget {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Center(
-        child: Text('Page 2'),
+      body: GestureDetector(
+        onTap: () {
+          Navigator.pop(context);
+        },
+        child: Center(
+          child: Hero( 
+            tag: 'Image Laptop',
+            child: Image.network('https://picsum.photos/250?image=9')
+          ),
+        ),
       ),
     );
   }
 }
-
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
 
